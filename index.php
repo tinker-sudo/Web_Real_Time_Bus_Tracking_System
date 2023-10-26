2b60c48f661ba29ac652e6aed33a4effb67ab671
SaiKou.Repo <mohdalifffarhanibrahim@gmail.com>
2023-10-26 13:47:27 +0800

    Added Bootstrap and Change of button

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
@@ -14,7 +15,7 @@
         <p>Please enter your name and age</p>
         <input type="text" name="name">
         <input type="number" name="age">
-        <button>Submit</button>
+        <button type="button" class="btn btn-primary">Submit</button>
 
     </form>
 
